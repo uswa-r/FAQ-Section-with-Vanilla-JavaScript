9fc619bc2226c9e7be191979281e198cf542198e
uswa.r <uswa.r@outlook.com>
2022-09-07 19:02:57 +0500

    .

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn');
     btn.addEventListener("click", function() {
 
-        questions.forEach("click", function(item) {
-            if (item !== question) {
-                item.classList.remove('show-text');
-            }
-        });
+        // questions.forEach("click", function(item) {
+        //     if (item !== question) {
+        //         item.classList.remove('show-text');
+        //     }
+        // });
         question.classList.toggle("show-text");
     });
 });
