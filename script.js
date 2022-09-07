f859b4453fa404783c0c0d9c12d43f7d363ec293
uswa.r <uswa.r@outlook.com>
2022-09-07 18:45:54 +0500

    updated

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const questions = document.querySelectorAll('question');
+const questions = document.querySelectorAll('.question');
 
 questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn');
     btn.addEventListener('click', function() {
+
+        questions.forEach("click", function(item) {})
         question.classList.toggle("show-text");
     });
 });
