fc862930ea2c250444434d3a7d5c8cc13d78f742
uswa.r <uswa.r@outlook.com>
2022-09-07 19:08:26 +0500

    done

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const questions = document.querySelectorAll('.question');
 questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn');
     btn.addEventListener("click", function() {
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        })
 
-        // questions.forEach("click", function(item) {
-        //     if (item !== question) {
-        //         item.classList.remove('show-text');
-        //     }
-        // });
         question.classList.toggle("show-text");
     });
 });
